268d7a8dbd127d87ff87d6290d5f4f52cbab41e8
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-11 14:08:19 +0500

    Set up the Sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <Wrapper>
       <h5>
-        &copy; {new Date().getFullYear()} &nbsp;
-         <span>ComfySloth</span>
+        &copy; {new Date().getFullYear()};
+         <span> ComfySloth</span>
       </h5>
       <h5>All rights reserved</h5>
     </Wrapper>
