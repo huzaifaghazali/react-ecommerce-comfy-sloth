af204c86f204105ede8c8689f38ac359f809bf62
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-13 13:21:17 +0500

    Set up the Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and 20% off</h3>
+        <div className='content'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
+          quam cupiditate mollitia minus quaerat numquam necessitatibus et velit
+          maxime quis.
+        </div>
+        <form className='contact-form'>
+          <input type='text' className='form-input' placeholder='enter email' />
+          <button className="submit-btn">subscribe</button>
+        </form>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +82,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
