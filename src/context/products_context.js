99f76c2ac0140601787435d08f9df48cd43ed0cf
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-13 13:50:23 +0500

    fetch the data using axios

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -30,6 +30,16 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
+
+  const fetchProducts = async(url) => {
+    const response = await axios.get(url);
+    console.log(response);
+  }
+
+  useEffect(() =>  {
+    fetchProducts(url)
+  }, [])
+
   return (
     <ProductsContext.Provider
       value={{
