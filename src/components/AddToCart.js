ebe69aa5edfd7c71f4e05f4b1cfc04e87fcdf8d5
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-15 22:41:01 +0500

    Add increase and decrease button in AddToCart component

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,6 +9,29 @@ const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount -  1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
+
+
 
   return (
     <Wrapper>
@@ -20,7 +43,9 @@ const AddToCart = ({ product }) => {
               <button
                 key={index}
                 style={{ background: color }}
-                className={`${mainColor === color ? 'color-btn active' : 'color-btn' }`}
+                className={`${
+                  mainColor === color ? 'color-btn active' : 'color-btn'
+                }`}
                 onClick={() => setMainColor(color)}
               >
                 {mainColor === color ? <FaCheck /> : null}
@@ -28,7 +53,16 @@ const AddToCart = ({ product }) => {
             );
           })}
         </div>
-        <div className='btn-container'></div>
+      </div>
+      <div className='btn-container'>
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to='/cart' className='btn'>
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   );
