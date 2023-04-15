ecdae8f9be285f85d57fed42eb1e3d1a47cc4145
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-15 17:39:05 +0500

    Set up the Single Product Page

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,6 @@ import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Product = ({ image, name, price, id }) => {
-  console.log(image);
   return (
     <Wrapper>
       <div className="container">
--- src/context/products_context.js
+++ src/context/products_context.js
@@ -72,6 +72,7 @@ export const ProductsProvider = ({ children }) => {
         ...state,
         openSidebar,
         closeSidebar,
+        fetchSingleProduct
       }}
     >
       {children}
