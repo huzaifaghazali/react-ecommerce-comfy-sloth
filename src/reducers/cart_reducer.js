2daebed3cb1b7713a09ffa362f32ebef3c44d61e
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-20 21:05:20 +0500

    Add count cart items and price functionality

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -56,37 +56,54 @@ const cart_reducer = (state, action) => {
     return { ...state, cart: [] };
   }
 
-  // ------ Increasing and decreasing cart item ------ 
-  if( action.type === TOGGLE_CART_ITEM_AMOUNT){
-    
-    const {id, value} = action.payload;
+  // ------ Increasing and decreasing cart item ------
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload;
     const tempCart = state.cart.map((item) => {
-      if(item.id === id) {
-        
+      if (item.id === id) {
         // Increase the Cart item
-        if(value === 'inc') {
+        if (value === 'inc') {
           let newAmount = item.amount + 1;
           // must not be greater than the amount
-          if(newAmount > item.max) {
+          if (newAmount > item.max) {
             newAmount = item.max;
           }
-          return {...item, amount: newAmount };
+          return { ...item, amount: newAmount };
         }
 
         // Decrease the cart item
-        if(value === 'dec') {
+        if (value === 'dec') {
           let newAmount = item.amount - 1;
           // must not be less than 1
-          if(newAmount < 1) {
+          if (newAmount < 1) {
             newAmount = 1;
           }
-          return {...item, amount: newAmount };
+          return { ...item, amount: newAmount };
         }
-      } 
+      }
       return item;
-    })
-    
+    });
+
     return { ...state, cart: tempCart };
+  }
+
+  // ------ Count Cart item  and Price ------
+  if (action.type === COUNT_CART_TOTALS) {
+    const { total_items, total_amount } = state.cart.reduce(
+      (total, cartItem) => {
+        const { amount, price } = cartItem;
+
+        total.total_items += amount;
+        total.total_amount += price * amount;
+
+        return total;
+      },
+      {
+        total_items: 0,
+        total_amount: 0,
+      }
+    );
+    return { ...state, total_items, total_amount };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
