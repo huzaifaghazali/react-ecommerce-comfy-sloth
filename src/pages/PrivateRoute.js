9751572a22dd0c0e231ef68b31852477a8ac306f
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-21 16:45:20 +0500

    Protect the checkout Route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -4,7 +4,18 @@ import { useAuth0 } from '@auth0/auth0-react';
 // will remove later
 import { useUserContext } from '../context/user_context';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ children, ...rest }) => {
+  const { myUser } = useUserContext();
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return myUser ? children : <Redirect to='/'></Redirect>;
+      }}
+    ></Route>
+  );
+
   return <h4>Private Route</h4>;
 };
 export default PrivateRoute;
